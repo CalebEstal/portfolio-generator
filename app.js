65385873705f0dde40742bc77f1023dc261e5984
Caleb E <mkcnj01@gmail.com>
2024-06-27 05:26:33 -0500

    added npm inquirer functionality

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-const fs = require('fs');
+import inquirer from "inquirer";
 
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-//console.log(profileDataArgs);
-
-//Notice the lack of parantheses around the 'ProfileDataArr' parameter?
-//const printProfileData = profileDataArr => {
-    //This...
-    //for (let i=0; i < profileDataArr.length; i++) {
-        //console.log(profileDataArr[i]);
-    //}
-
-    //console.log('==============');
-
-    //Is the same as this....
-    //profileDataArr.forEach(profileItem => console.log(profileItem));
-//};
-
-//printProfileData(profileDataArgs);
-
-//=============================================================================================================
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
 
-    console.log('Portfilio Complete! Check out index.html to see the output!')
-});
+//const fs = require('fs');
+
+//const generatePage = require('./src/page-template.js');
+
+
+//const pageHTML = generatePage(name, github);
+
+
+//fs.writeFile('./index.html', pageHTML, err => {
+    //if (err) throw err;
+
+    //console.log('Portfilio Complete! Check out index.html to see the output!')
+//});
