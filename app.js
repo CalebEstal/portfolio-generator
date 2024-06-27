2c8f5c1e4fdd4356e5eb61a47acb4f194cfb3072
Caleb E <mkcnj01@gmail.com>
2024-06-27 03:39:47 -0500

    generated html file and modularized code

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+//console.log(profileDataArgs);
 
 //Notice the lack of parantheses around the 'ProfileDataArr' parameter?
-const printProfileData = profileDataArr => {
+//const printProfileData = profileDataArr => {
     //This...
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+    //for (let i=0; i < profileDataArr.length; i++) {
+        //console.log(profileDataArr[i]);
+    //}
 
-    console.log('==============');
+    //console.log('==============');
 
     //Is the same as this....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+    //profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
+
+//=============================================================================================================
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfilio Complete! Check out index.html to see the output!')
+});
