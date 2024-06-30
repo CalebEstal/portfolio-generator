cdd572b273869174169856e785425a1db2fe3c6a
Caleb E <mkcnj01@gmail.com>
2024-06-30 10:18:23 -0500

    finished project

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import inquirer from "inquirer";
+import generatePage from"./src/page-template.js";
+import { writeFile } from"./utils/generate-site.js";
+import { copyFile } from"./utils/generate-site.js";
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -138,21 +141,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
-
-
-
-//const fs = require('fs');
-
-//const generatePage = require('./src/page-template.js');
-
-
-//const pageHTML = generatePage(name, github);
-
-
-//fs.writeFile('./index.html', pageHTML, err => {
-    //if (err) throw err;
-
-    //console.log('Portfilio Complete! Check out index.html to see the output!')
-//});
